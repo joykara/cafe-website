56bc548f01e8e4694c321cc49d042fa8384c1f85
joykara <joykarani1@gmail.com>
2023-08-15 21:16:41 +0300

    edit

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Navbar from '../components/navbar/Navbar'
 import Header from '../components/header/Header'
+import { About, Footer, Gallery, Products, Review } from '../components'
 
 const Homepage = () => {
   return (
     <>
       <Navbar />
       <Header />
+      <About />
+      <Gallery />
+      <Products />
+      <Review />
+      <Footer />
     </>
   )
 }

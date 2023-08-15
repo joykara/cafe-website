0b35cfe0bc91daa4b5e144157f4e87dde731bcf2
joykara <joykarani1@gmail.com>
2023-08-15 21:15:12 +0300

    upd navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react';
+import './navbar.css';
+import { RiMenuLine, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/logo.jpeg';
 
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+    const handleToggle = () => {
+        setToggleMenu(!toggleMenu)
+    }
+
     return (
       <div className="navbar-container">
             <div className="navbar-logo">
-                <img src="https://www.docplanner.com/img/logo-default-group-en.svg?v=1" alt="Docplanner Group" />
+                <img src={logo} alt="Coffee Connection" />
             </div>
-            <div className="navbar-links">
-                <ul>
-                    <li><a href="/">ABOUT US</a></li>
+            <div>
+                <ul className="navbar-links">
+                    <li><a href="#about-us">ABOUT US</a></li>
                     <li><a href="/">COFFEE</a></li>
                     <li><a href="/">MENU</a></li>
-                    <li><a href="/">CAREERS</a></li>
+                    <li><a href="#gallery">GALLERY</a></li>
                     <li><a href="/">CONTACT</a></li>
                     <li><button type='submit'>Order NOW!</button></li>
                 </ul>
+            </div>
+            {/* hamburger navbar */}
+            <div className={`navbar-toggle ${toggleMenu ? 'active' : ''}`} onClick={handleToggle}>
+                {toggleMenu ? <RiCloseLine size={25} color='white'/> : <RiMenuLine size={25} color='white'/>}
+                {toggleMenu && (
+                <nav className="navbar-menu-mobile">
+                    <ul>
+                        <li><a href="#about-us">ABOUT US</a></li>
+                        <li><a href="/">COFFEE</a></li>
+                        <li><a href="/">MENU</a></li>
+                        <li><a href="#gallery">GALLERY</a></li>
+                        <li><a href="/">CONTACT</a></li>
+                        <li><button type='submit'>Order NOW!</button></li>
+                    </ul>
+                </nav>
+                )}
             </div>
       </div>
   )

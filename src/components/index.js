bbbd26cfe89ace2b97f9441d5f941d6796a8c811
joykara <joykarani1@gmail.com>
2023-08-15 21:16:34 +0300

    export components

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,7 @@
 export { default as Header } from './header/Header';
 export { default as Footer } from './footer/Footer';
 export { default as Navbar } from './navbar/Navbar';
+export { default as About } from './about/About';
+export { default as Gallery } from './gallery/Gallery';
+export { default as Products } from './products/Products';
+export { default as Review } from './reviews/Review';

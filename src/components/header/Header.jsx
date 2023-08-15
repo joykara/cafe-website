a1fb4c193ecab47518b5e2d497fdb2dfd4f2eb1c
joykara <joykarani1@gmail.com>
2023-08-15 21:14:53 +0300

    upd header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import './header.css';
 
 const Header = () => {
     return (
@@ -10,6 +11,21 @@ const Header = () => {
             <li>WOONSOCKET</li>
             <li>NORTH PARK</li>
           </ul>
+        </div>
+        <div className="header-details">
+          <div className="header-details-left">
+            <h2>Hours</h2>
+            <ul>
+              <li>Monday - Friday: 6am - 6pm</li>
+              <li>Saturday: 7am - 6pm</li>
+              <li>Sunday: 7am - 5pm</li>
+            </ul>
+          </div>
+          <div className="header-details-right">
+            <h2>Address</h2>
+            <p>725 Mendon Rd, Cumberland, RI 02864</p>
+            <p>401-333-3333</p>
+          </div>
         </div>
       </div>
   )
